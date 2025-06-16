5ed9161fd1c933cd4c5245b5ebd2ba244338128f
sam1926 <62940328+sam1926@users.noreply.github.com>
2025-06-16 22:11:38 +1000

    Update logo.tsx

--- client/src/components/logo.tsx
+++ client/src/components/logo.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import favicon from './favicon-32x32.png'; // Adjust path if needed
 
 export default function Logo() {
   return (
     <div className="logo-container w-12 h-12 relative block">
       <img 
-        src="/favicon-32x32.png" 
+        src={favicon} 
         alt="Logo" 
         className="w-full h-full object-contain" 
       />

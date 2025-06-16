5cd95fac8d90713f513cb503b44835c667a1f8d9
sam1926 <62940328+sam1926@users.noreply.github.com>
2025-06-16 22:14:25 +1000

    Update logo.tsx

--- client/src/components/logo.tsx
+++ client/src/components/logo.tsx
@@ -3,7 +3,7 @@ import favicon from './zynlologo.png'; // Adjust path if needed
 
 export default function Logo() {
   return (
-    <div className="logo-container w-12 h-12 relative block">
+    <div className="logo-container w-10 h-10 relative block">
       <img 
         src={favicon} 
         alt="Logo" 

541f9da06471afe647d10a460ce51d499383022b
sam1926 <62940328+sam1926@users.noreply.github.com>
2025-06-16 22:08:20 +1000

    Update logo.tsx

--- client/src/components/logo.tsx
+++ client/src/components/logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <div className="logo-container w-12 h-12 relative block">
       <img 
-        src="./favicon-32x32.png" 
+        src="/favicon-32x32.png" 
         alt="Logo" 
         className="w-full h-full object-contain" 
       />
